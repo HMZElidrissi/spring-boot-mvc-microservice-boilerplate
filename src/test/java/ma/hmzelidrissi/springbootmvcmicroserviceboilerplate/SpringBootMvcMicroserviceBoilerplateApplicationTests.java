8851b74eb5c66df4571d35d34f688fd679dcdda0
hmzelidrissi <hamza.ezzharelidrissi1@gmail.com>
2025-01-22 16:15:36 +0100

    setup project

---
+++ src/test/java/ma/hmzelidrissi/springbootmvcmicroserviceboilerplate/SpringBootMvcMicroserviceBoilerplateApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.hmzelidrissi.springbootmvcmicroserviceboilerplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootMvcMicroserviceBoilerplateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
